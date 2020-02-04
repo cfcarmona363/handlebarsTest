0f19a3b5e2f541f40bdace414ec251c5cbf790c2
Francisco <francisco@eurekalabs.io>
2020-02-04 11:25:59 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express')
+const exhbs=require('express-handlebars')
+const app = express()
+
+
+app.engine('handlebars', exhbs({
+  defaultLayout:'main'
+}))
+
+app.set('view engine', 'handlebars' )
+
+app.get('/', (req, res)=>{
+  res.render('index',{
+    greeting: "Welcome to my Home Page"
+  })
+})
+
+app.get('/about', (req, res)=>{
+  res.render('about',{
+    greeting: "Welcome to my About Page"
+  })
+})
+
+const port=5005
+
+app.listen(port, ()=>{
+  console.log(`server started on ${port}`)
+})
